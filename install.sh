d5e15902e4194d3bf63c6e5ceea622c4fcf8dec8
pachinko2821 <pachinko28@protonmail.com>
2021-11-05 21:45:50 +0530

    Added install script (INCOMPLETE)

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir dependencies
+
+git submodule add https://github.com/GerbenJavado/LinkFinder dependencies/LinkFinder
+git submodule add https://github.com/devanshbatham/ParamSpider dependencies/Paramspider
+git submodule add https://github.com/six2dez/degoogle_hunter dependencies/degoogle_hunter
